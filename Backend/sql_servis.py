0ccaebf6691702c628f0696b7189f5032b3e7475
Berkan Eti <berkaneti34@gmail.com>
2024-01-01 18:33:15 +0300

    ...

--- Backend/sql_servis.py
+++ Backend/sql_servis.py
@@ -3,6 +3,8 @@ import Backend.Helper as Helper
 from Backend.Entities import *
 from psycopg2.errors import *
 
+yeni 
+
 Helper.DataBaseConnector.singleton = Helper.DataBaseConnector()
 def registerEmployer(employer,account):
     if(employer.employerName == None) :
