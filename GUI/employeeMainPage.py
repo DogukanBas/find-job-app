07c0b8738014dd9129a2ce11d716083149b8fb06
Bilal Müftüoğlu <bilalmuftuoglu@hotmail.com>
2024-01-04 19:29:36 +0300

    sql view eklendi

--- GUI/employeeMainPage.py
+++ GUI/employeeMainPage.py
@@ -13,7 +13,7 @@ def showPage(employeeId):
     tab1 = ttk.Frame(tabControl) 
     tab2 = ttk.Frame(tabControl)
     
-    tabControl.add(tab1, text ='Advertisements') 
+    tabControl.add(tab1, text ='Applications') 
     tabControl.add(tab2, text ='Profile') 
     tabControl.pack(expand = 1, fill ="both") 
      
